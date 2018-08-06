c7810197630b4e1eba74a102c8e518ffb843b196
zongfi <zhzephizhao@huangbaoche.com>
2018-08-06 11:18:48 +0800

    语音识别

---
+++ said/src/main/java/tk/hongbo/said/MainActivity.kt
@@ -0,0 +1,45 @@
+package tk.hongbo.said
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initPermission() //授权
+    }
+
+    /**
+     * android 6.0 以上需要动态申请权限
+     */
+    private fun initPermission() {
+        val permissions = arrayOf(Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.INTERNET,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        val toApplyList = ArrayList<String>()
+
+        for (perm in permissions) {
+            if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, perm)) {
+                toApplyList.add(perm);
+                //进入到这里代表没有权限.
+            }
+        }
+        val tmpList = arrayOfNulls<String>(toApplyList.size);
+        if (!toApplyList.isEmpty()) {
+            ActivityCompat.requestPermissions(this, toApplyList.toArray(tmpList), 123);
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        // 此处为android 6.0以上动态授权的回调，用户自行实现。
+    }
+}
